7206f4bb29b248d5f27850b6024318548dad2a1d
scrabill <shannon.crabill@gmail.com>
2019-09-30 21:33:10 -0400

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,31 @@
 class UsersController < ApplicationController
 
 
+  get '/login' do
+    erb :'/users/login'
+  end
+
+  get '/signup' do
+   if Helpers.is_logged_in?(session)
+     redirect "/tweets"
+   end
+
+  erb :'/users/signup'
+
+  end
+
+  post '/signup' do
+  # binding.pry {"username"=>"skittles123", "email"=>"skittles@aol.com", "password"=>"rainbows"}
+
+    params.each do |key, value|
+      if value.empty?
+        redirect "/signup"
+      end
+    end
+
+    user = User.create(params)
+    session[:user_id] = user.id
+    redirect "/tweets"
+  end
+
 end
